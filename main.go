84a08ad5e89d8d0b4b4ae4ac6bfae7f6f75ceef5
niehengyang <niehengyang@ebupt.com>
2021-06-17 15:11:47 +0800

    基础账号权限系统搭建完毕

--- main.go
+++ main.go
@@ -12,7 +12,14 @@ var rootCmd = &cobra.Command{
 	Use:   "lets",
 	Short: "lets 业务框架命令",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("请指定要运行的子命令")
+		fmt.Printf("---------------指令集-----------------\n")
+		fmt.Printf("|          api  -> 接口服务           |\n")
+		fmt.Printf("--------------------------------------\n")
+		fmt.Printf("|          rbmq -> 队列服务           |\n")
+		fmt.Printf("--------------------------------------\n")
+		fmt.Printf("|          mqtt -> 数据服务           |\n")
+		fmt.Printf("--------------------------------------\n")
+		fmt.Printf("请指定要运行的子命令:\n")
 	},
 }
 
